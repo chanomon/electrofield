31679d835fdc263a829685a65f5857a58cb1397c
Elizandro <hdomeli@gmail.com>
2019-09-27 17:35:53 -0500

    dipole implemented

--- vectorfield.py
+++ vectorfield.py
@@ -10,34 +10,43 @@ from matplotlib.patches import Circle
 # v is the position of q
 k = 9e9 #N * m**2 / C**2
 def Electrostaticfield(u,q,X,Y):
-    q1 = 1.
-    x0 = u[0]
+    #q0 = -1.
+    x0 = u[0] + 1.
     y0 = u[1]
-    
-
-
-    #asdf
-    r_vecx = x-x0
-    r_vecy = y-y0
+    r_vecx = X-x0
+    r_vecy = Y-y0
     r = np.hypot(r_vecx,r_vecy)
     
 #    r = np.sqrt(r_vec.dot(r_vec))# mod(v-u)
     
-    E = [k*( q /r**3) * (r_vec_x) , k*( q /r**3) * (r_vec_y)]
+    E0 = [k*( q /r**3) * (r_vecx) , k*( q /r**3) * (r_vecy)]
+
+
+    x1 = u[0]
+    y1 = u[1]
+    r_vecx = X - x1
+    r_vecy = Y - y1
+        
+    r = np.hypot(r_vecx,r_vecy)
+    E = [E0[0] + k * (-q / r**3) * (r_vecx), E0[1] + k * (-q / r**3) * (r_vecy)]
+    
     return E
 
-u = np.array([0,0,0])
+u = np.array([-0.5,0.])
 q = 1.
 
 nx, ny = 64, 64
-x = np.linspace(-2,2,nx)
-y = np.linspace(-2,2,ny)
+x = np.linspace(-1,1,nx)
+y = np.linspace(-1,1,ny)
 X, Y = np.meshgrid(x,y)
 
 #field = []
 
-[Ex, Ey] = electrostaticfield(u,q,[x,y])
-
+[Ex, Ey] = Electrostaticfield(u,q,X,Y)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.streamplot(x,y,Ex, Ey, linewidth = 1 , cmap = plt.cm.inferno, density = 2 , arrowstyle = "->", arrowsize = 2)
+plt.show()
 #v = [1e-6,0,0]
 #F = Electrostaticfield(u,q,v)
 #print F
