73ddfda76a86cfd7bed8efc1eb41832823470ffd
chanomon <hdomeli@gmail.com>
2019-09-27 12:01:08 -0500

    actualizando

--- vectorfield.py
+++ vectorfield.py
@@ -9,13 +9,22 @@ from matplotlib.patches import Circle
 #  q is charge
 # v is the position of q
 k = 9e9 #N * m**2 / C**2
-def Electrostaticforce(u,q,v):
+def Electrostaticfield(u,q,X,Y):
     q1 = 1.
-    r_vec = v - u 
-    r = np.sqrt(r_vec.dot(r_vec))# mod(v-u)
+    x0 = u[0]
+    y0 = u[1]
     
-    F = (k  * q1 * q /r**3)*r_vec
-    return F
+
+
+    #asdf
+    r_vecx = x-x0
+    r_vecy = y-y0
+    r = np.hypot(r_vecx,r_vecy)
+    
+#    r = np.sqrt(r_vec.dot(r_vec))# mod(v-u)
+    
+    E = [k*( q /r**3) * (r_vec_x) , k*( q /r**3) * (r_vec_y)]
+    return E
 
 u = np.array([0,0,0])
 q = 1.
@@ -23,15 +32,15 @@ q = 1.
 nx, ny = 64, 64
 x = np.linspace(-2,2,nx)
 y = np.linspace(-2,2,ny)
+X, Y = np.meshgrid(x,y)
 
-field = []
-for a in x:
-    for b in y:
-        field.append(electrostaticforce(u,q,[a,b]))
+#field = []
 
-v = [1e-6,0,0]
-F = Electrostaticforce(u,q,v)
-print F
+[Ex, Ey] = electrostaticfield(u,q,[x,y])
+
+#v = [1e-6,0,0]
+#F = Electrostaticfield(u,q,v)
+#print F
 
 
     
